b7bb89c1e6f803f07ab207a25950f9d6ed22399e
batoi <igomis@cipfpbatoi.es>
2023-03-09 18:07:01 +0100

    Login

---
+++ php/src/classes/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class User
+{
+    public static function login($email, $password){
+        $connection = new Connection();
+    }
+}
--- php/src/login.php
+++ php/src/login.php
@@ -1,14 +1,12 @@
 <?php
 include_once('../vendor/autoload.php');
-use App\Connection;
+use App\User;
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $connection = new Connection();
-    $connection->insert('users', [
-        'email' => $email,
-        'password' => $password
-    ]);
+
+    $user = User::login($email,$password);
+
     header('Location: ../index.php');
 }
